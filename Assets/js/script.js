39b849ea3553790d84c2a0523069aface6a6edbd
elle-chichi <mchellechi@gmail.com>
2024-10-17 21:41:46 +0300

    initial commit

---
+++ Assets/js/script.js
@@ -0,0 +1,6 @@
+const colorButton=document.getElementById('colorButton');
+
+colorButton.addEventListener('click',()=>{
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor === 'blue'? 'darkred':'blue';
+});
+
